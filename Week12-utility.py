704f613e86e1c60d1254bbc69cb6a4d35ec3ff5b
Sir-Slimestone <47618065+Sir-Slimestone@users.noreply.github.com>
2019-11-21 11:03:10 -0700

    Add files via upload

--- Week12-utility.py
+++ Week12-utility.py
@@ -11,4 +11,16 @@ def LoadFile(file):
 def UpdateString(string1, string2, index):
     out = string1[0:index] + string2 + string1[index+1:-1] + string1[-1]
     print("OUTPUT " + out)
+def FindWordCount(listin, string):
+    count = 0
+    for i in listin:
+        find = True
+        check = i
+        while find:
+            if string in check:
+                count += 1
+                check = check[check.find(string)+1:-1] + check[-1]
+            else:
+                find = False
+    return count
 
