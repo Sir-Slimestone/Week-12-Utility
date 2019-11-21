5e155318c268aa7b02396603da4cc52accc5db3d
Sir-Slimestone <47618065+Sir-Slimestone@users.noreply.github.com>
2019-11-21 10:49:23 -0700

    Add files via upload

--- Week12-utility.py
+++ Week12-utility.py
@@ -3,4 +3,9 @@
 #Week 12 Part A
 def PrintOutput(string):
     print("OUTPUT " + string)
+def LoadFile(file):
+    text = open(file, "r")
+    out = text.readlines()
+    text.close()
+    return out
 
